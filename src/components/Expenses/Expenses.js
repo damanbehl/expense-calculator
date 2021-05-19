fc8fd13adcd8c9d2de81284f928ee237018b83d5
Daman Behl <damanbehl@gmail.com>
2021-05-19 02:42:05 -0400

    filter 2 way binding + raise state + bottom up

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,42 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
   const expenses = props.expenses;
+  const [selectedFYear, setSelectedFYear] = useState("2019");
+  const selectedFYearHandler = (selectedYear) => {
+    setSelectedFYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-    </Card>
+    // <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={selectedFYear} onSelectFilterYear={selectedFYearHandler} />
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        />
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        />
+        <ExpenseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        />
+        <ExpenseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        />
+      </Card>
+    // </div>
   );
-}
+};
 
 export default Expenses;
