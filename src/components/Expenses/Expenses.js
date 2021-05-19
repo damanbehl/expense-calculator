636c2dcdbcd5c8f3a9e8e5ebd0f365f5c6fae561
Daman Behl <damanbehl@gmail.com>
2021-05-19 03:26:29 -0400

    section four done

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,33 +8,38 @@ const Expenses = (props) => {
   const expenses = props.expenses;
   const [selectedFYear, setSelectedFYear] = useState("2019");
   const selectedFYearHandler = (selectedYear) => {
+    console.log("expenses.js");
+    console.log(selectedYear);
     setSelectedFYear(selectedYear);
   };
   return (
     // <div>
-      <Card className="expenses">
-        <ExpensesFilter selected={selectedFYear} onSelectFilterYear={selectedFYearHandler} />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
-      </Card>
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={selectedFYear}
+        onSelectFilterYear={selectedFYearHandler}
+      />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </Card>
     // </div>
   );
 };
