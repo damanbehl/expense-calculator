b7f35694aaa62a3dd36b36237168e5ec054ad8d9
Daman Behl <damanbehl@gmail.com>
2021-05-20 03:04:07 -0400

    pre charts + form toggle added

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,32 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm.js";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [formState, setFormState] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setFormState(false);
+  };
+  const toggleFormState = () => {
+    setFormState(true);
+  };
+  const toggleFormFalse = () => {
+    setFormState(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!formState && <button onClick={toggleFormState}>Add New Expense</button>}
+      {formState && (
+        <ExpenseForm
+          onCancel={toggleFormFalse}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      )}
     </div>
   );
 };
